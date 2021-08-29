3a9c49e2fdce584fab06d07cb2e7188d8f7f2a26
kariswr <karinaiswara.ki@gmail.com>
2021-08-29 23:30:51 +0700

    move browser router

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import Home from './components/Home';
 import Questions from './components/Questions';
 import {Route, Switch} from 'react-router';
-import {BrowserRouter} from 'react-router-dom';
 
 class App extends React.Component {
   render(){
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/questions" component={Questions} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/questions" component={Questions} />
+        </Switch>
+      </div>
     );
   }
 }
