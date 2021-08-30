da1695f2e0cb604fe503de9ade6936d21951ad81
kariswr <karinaiswara.ki@gmail.com>
2021-08-31 02:01:29 +0700

    add dummy paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -107,8 +107,11 @@ class Home extends React.Component {
           <Container className="fluid p-10 my-3">
             <Row>
               <h3 className='top-padding'> Sistem Evaluasi Kemampuan Mandiri </h3>
+              <div className='bottom-padding'> 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </div>
             </Row>
-              <Row>
+            <Row>
               <Form>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                   <Form.Label>Masukan paragraf :</Form.Label>
